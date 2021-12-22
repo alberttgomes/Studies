a4982cb23c7919b4bcc4d1096aa19302c4b8e75a
alberttgomes <albertgomes1995@hotmail.com>
2021-12-22 00:17:36 -0300

    .NET first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Studies
+{
+   class Program
+   {
+    static void Main (String[] args)
+    {
+        int numeroVezes = 5;
+
+        for(int i = 0; i < numeroVezes; i++)
+        {
+            Console.WriteLine($"Studies .NET Microsoft, Language C# {i}");
+        }
+    } 
+
+   }
+ 
+}
