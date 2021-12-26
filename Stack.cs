7abf84a8dd2d978056afb335e2fdfeaf9f84ebe8
alberttgomes <albertgomes1995@hotmail.com>
2021-12-26 19:36:39 -0300

    stack with  dotnet

---
+++ Stack.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace StructurePilha
+{
+    public class Pilha
+    {
+        Position first;
+
+        public void Stack(object item)
+        {
+            first = new Position(first, item);
+        }
+
+        public object Unstacks()
+        {
+            if(first == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object result = first.item;
+            first = first.next;
+            
+            return result;
+        }
+    }
+
+    class Position
+    {
+        public Position next;
+        public  object item;
+        public Position(Position next, object item)
+        {
+            this.next = next;
+            this.item = item;
+        }
+    }
+    
+}
