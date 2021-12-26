6de75a80ab86b40bced82930825de193668d2505
alberttgomes <albertgomes1995@hotmail.com>
2021-12-26 19:52:35 -0300

    inicializando pilha

--- Program.cs
+++ Program.cs
@@ -8,10 +8,20 @@ namespace StructureStack
         static void Main ()
         {
            var s = new Pilha();
+          
            s.Stack(1);
            s.Stack(10);
            s.Stack(100);
            s.Stack(1000);
+           Console.WriteLine(s.Unstacks());
+           Console.WriteLine(s.Unstacks());
+           Console.WriteLine(s.Unstacks());
+           Console.WriteLine(s.Unstacks());
+
+           s.Stack(1000);
+           s.Stack(100);
+           s.Stack(10);
+           s.Stack(1);
            Console.WriteLine(s.Unstacks());
            Console.WriteLine(s.Unstacks());
            Console.WriteLine(s.Unstacks());
