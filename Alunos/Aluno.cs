de143968477ea34f4bf46e2596ca8da2e576ca2e
alberttgomes <albertgomes1995@hotmail.com>
2022-01-13 01:21:36 -0300

    sistema para cadastro de alunos e notas

---
+++ Alunos/Aluno.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlunoEx
+{
+    public class Aluno
+    {
+        public string? Nome {get; set; }
+        
+        public decimal? Nota {get; set; }
+    }
+}
---
+++ Alunos/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using AlunoEx;
+
+namespace Alunos
+ {
+     class Program
+     {
+         static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do Aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota: ");
+                        
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                           aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser décimal");
+                        }
+                        
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if(!a.Nome.Equals(""))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} -  Nota: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            opcaoUsuario = ObterOpcaoUsuario();
+        
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+            
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+ }
